ac37c0355e9383ab4d14293c1de9383ec8603836
Feny Arifhienta <fenyarifhienta@gmail.com>
2016-12-05 20:34:23 +0700

    Android

---
+++ Curify/app/src/main/java/com/example/user/curify/FormLoad.java
@@ -0,0 +1,13 @@
+package com.example.user.curify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormLoad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_load);
+    }
+}
